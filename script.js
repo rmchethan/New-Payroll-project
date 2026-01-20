e585ad78bda41940703ea87eeb890314d77e9b3d
Chethan Murali <90843010+rmchethan@users.noreply.github.com>
2026-01-20 12:22:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,25 +24,5 @@ let lohnsteuer = gesamtBrutto * 0.20;
 // Netto
 let netto = gesamtBrutto - lohnsteuer - sozialversicherung - jobticket;
 
-// Ausgabe
-document.getElementById("output").innerHTML =
-  "<strong>Brutto Bestandteile</strong><br>" +
-  "Grundgehalt: " + brutto.toFixed(2) + " €<br>" +
-  "Nachtschichtzuschlag: " + Nachtschichtzuschlag.toFixed(2) + " €<br>" +
-  "Ferientagzuschlag: " + Ferientagzuschlag.toFixed(2) + " €<br>" +
-  "Sonntagzschlag: " + Sonntagzschlag.toFixed(2) + " €<br>" +
-  "Überstunden: " + ueberstunden.toFixed(2) + " €<br>" +
-  "<strong>Gesamtbrutto: " + gesamtBrutto.toFixed(2) + " €</strong><br><br>" +
-
-  "<strong>Abzüge</strong><br>" +
-  "Lohnsteuer: " + lohnsteuer.toFixed(2) + " €<br>" +
-  "Krankenversicherung (KV): " + kv.toFixed(2) + " €<br>" +
-  "Rentenversicherung (RV): " + rv.toFixed(2) + " €<br>" +
-  "Arbeitslosenversicherung (AV): " + av.toFixed(2) + " €<br>" +
-  "Pflegeversicherung (PV): " + pv.toFixed(2) + " €<br>" +
-  "Jobticket: " + jobticket.toFixed(2) + " €<br><br>" +
-
-  "<strong>Netto: " + netto.toFixed(2) + " €</strong>";
-
-
 }
+
