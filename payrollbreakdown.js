f880ac2c0cf3c2777a76ef64f337d3564e8e9826
Chethan Murali <90843010+rmchethan@users.noreply.github.com>
2026-01-20 12:19:23 +0100

    Update payrollbreakdown.js

--- payrollbreakdown.js
+++ payrollbreakdown.js
@@ -1,5 +1,17 @@
 document.getElementById("output").innerHTML =
-  "Brutto: " + brutto + " €<br>" +
-  "Lohnsteuer (20%): " + steuer + " €<br>" +
-  "Sozialversicherung (20%): " + sozial + " €<br>" +
+  "<strong>Brutto Bestandteile</strong><br>" +
+  "Grundgehalt: " + brutto.toFixed(2) + " €<br>" +
+  "Zuschläge: " + zuschlaege.toFixed(2) + " €<br>" +
+  "Überstunden: " + ueberstunden.toFixed(2) + " €<br>" +
+  "NFS-Zuschläge: " + nfs.toFixed(2) + " €<br>" +
+  "<strong>Gesamtbrutto: " + gesamtBrutto.toFixed(2) + " €</strong><br><br>" +
+
+  "<strong>Abzüge</strong><br>" +
+  "Lohnsteuer: " + lohnsteuer.toFixed(2) + " €<br>" +
+  "Krankenversicherung (KV): " + kv.toFixed(2) + " €<br>" +
+  "Rentenversicherung (RV): " + rv.toFixed(2) + " €<br>" +
+  "Arbeitslosenversicherung (AV): " + av.toFixed(2) + " €<br>" +
+  "Pflegeversicherung (PV): " + pv.toFixed(2) + " €<br>" +
+  "Jobticket: " + jobticket.toFixed(2) + " €<br><br>" +
+
   "<strong>Netto: " + netto.toFixed(2) + " €</strong>";
