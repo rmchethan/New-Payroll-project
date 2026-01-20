92bac612713903ac3a2095bea3726677f3e5156f
Chethan Murali <90843010+rmchethan@users.noreply.github.com>
2026-01-20 11:57:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function calculateNetto() {
 
   let brutto = Number(document.getElementById("brutto").value);
-  let zuschlaege = Number(document.getElementById("zuschlaege").value);
+  let Überstunden = Number(document.getElementById("Überstunden").value);
+  let Nachtschichtzuschlag = Number(document.getElementById("Nachtschichtzuschlag").value)
+  let Ferientagzuschlag = Number(document.getElementById("Ferientagzuschlag").value)
+  let Sonntagzschlag = Number(document.getElementById("Sonntagzschlag").value)
+  let jobticket = Number(document.getElementById("jobticket").value);
 
   let steuer = brutto * 0.20;
   let sozial = brutto * 0.20;
@@ -17,4 +21,5 @@ let employeeType = document.getElementById("employeeType").value;
 if (employeeType === "normal") {
   steuer = brutto * 0.20;
   sozial = brutto * 0.20;
+
 }
